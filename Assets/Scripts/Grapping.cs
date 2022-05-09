5d8144f29d59f4b8ffdf0b9186812414057d80cb
Hồ Hải Đăng <72792170+hohaidang2000@users.noreply.github.com>
2022-05-09 17:16:27 +0700

    Basic snap on grid

--- Assets/Scripts/Grapping.cs
+++ Assets/Scripts/Grapping.cs
@@ -8,11 +8,14 @@ public class Grapping : MonoBehaviour
 {
     //private GrapInput grapInput;
     private Transform _selected;
+    private Transform _gridSelected;
     private bool _selecting = false;
+    private bool _releaseable = false;
     private float mZCoord;
     private Vector3 _mOffSet;
     private Camera camera;
     private Ray ray;
+    private Vector3 _oldPosition;
     private Vector3 mousePressDownPos;
     private Vector3 mouseReleasePos;
     // Start is called before the first frame update
@@ -26,30 +29,28 @@ public class Grapping : MonoBehaviour
     }
     private void OnMouseDown()
     {
-        Debug.Log("ok");
+        
+           _oldPosition = gameObject.transform.position;
         mousePressDownPos = Input.mousePosition;
         if (_selecting == false)
         {
-            Vector2 pos = mousePressDownPos;
-            ray = camera.ScreenPointToRay(pos);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                Transform selection = hit.transform;
-                if (selection.tag == "Chess")
-                {
-                    _selecting = true;
-                    _selected = hit.transform;
-                }
-            }
+            
+            _selecting = true;
+            _selected = gameObject.transform;
+            mZCoord = camera.WorldToScreenPoint(_selected.transform.position).z;
+            mZCoord -= 1;
+            _releaseable = false;
+
+
         }
+        
     }
 
     private void MovingSelectedOldInput()
     {
         if (_selecting == true)
         {
-            mZCoord = camera.WorldToScreenPoint(_selected.transform.position).z;
+            
             Vector2 pos = Input.mousePosition;
             Vector3 mousePositionForObject = new Vector3(pos.x, pos.y, mZCoord);
             _mOffSet = camera.ScreenToWorldPoint(mousePositionForObject);
@@ -58,70 +59,100 @@ public class Grapping : MonoBehaviour
     }
     private void OnMouseUp()
     {
+        OnMouseReleaseOldInput();
         mouseReleasePos = Input.mousePosition;
         _selecting = false;
-
-    }
-    /*
-    private void OnLeftClickRelease(InputAction.CallbackContext context)
-    {
-        _selecting = false;
-        UpgradCollideCheck();
-        //_selected = null;
-    }
-    private void UpgradCollideCheck()
-    {
-        Collider[] hitColliders = Physics.OverlapSphere(_selected.transform.position, 40);
-       
-        float distance = 9000;
-        int c = -1 ;
+        gameObject.transform.position = _gridSelected.transform.position;
         
-        for (int i =0; i< hitColliders.Length; i++)
+    }
+
+    private void OnMouseReleaseOldInput()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(_selected.transform.position,20);
+        Transform temp = gameObject.transform;
+        float distance = 99999;
+      
+
+        for (int i = 0; i < hitColliders.Length; i++)
         {
-            if (hitColliders[i].tag == "Chess" && hitColliders[i].name != _selected.name)
+            if (hitColliders[i].tag == "Grid" && hitColliders[i].GetComponent<Grid>()._occupied == false)
             {
                 float newDistance = (_selected.transform.position - hitColliders[i].transform.position).magnitude;
-                if(newDistance< distance)
+                if (newDistance < distance)
                 {
-                        distance = newDistance;
-                        c = i;
+                    distance = newDistance;
+                    _releaseable = true;
+                    temp = hitColliders[i].transform;
 
                 }
             }
-        }   
-        if(c >=0 )
-        {
-            hitColliders[c].GetComponent<SwitchingLevels>().CombineChess(_selected);
         }
-       
+        if(_releaseable)
+            _gridSelected = temp;
+
+
     }
-    private void OnLeftClick(InputAction.CallbackContext context)
-    {
-        Vector2 pos = grapInput.Mouse.Position.ReadValue<Vector2>();
-        ray = camera.ScreenPointToRay(pos);
-        RaycastHit hit;
-        if( Physics.Raycast(ray, out hit))
-        {
-            Transform selection = hit.transform;
-            if (selection.tag == "Chess")
-            {
-                _selecting = true;
-                _selected = hit.transform;
-            }
-        }
-    }
-    private void MovingSelected()
-    {
-        if (_selecting == true)
-        {
-            mZCoord =camera.WorldToScreenPoint(_selected.transform.position).z;
-            Vector2 pos = grapInput.Mouse.Position.ReadValue<Vector2>();
-            Vector3 mousePosition = new Vector3(pos.x, pos.y, mZCoord);
-            _mOffSet = camera.ScreenToWorldPoint(mousePosition);
-            _selected.transform.position =  _mOffSet;
-        }    
-    }
-    */
+
+    /*
+private void OnLeftClickRelease(InputAction.CallbackContext context)
+{
+   _selecting = false;
+   UpgradCollideCheck();
+   //_selected = null;
+}
+private void UpgradCollideCheck()
+{
+   Collider[] hitColliders = Physics.OverlapSphere(_selected.transform.position, 40);
+
+   float distance = 9000;
+   int c = -1 ;
+
+   for (int i =0; i< hitColliders.Length; i++)
+   {
+       if (hitColliders[i].tag == "Chess" && hitColliders[i].name != _selected.name)
+       {
+           float newDistance = (_selected.transform.position - hitColliders[i].transform.position).magnitude;
+           if(newDistance< distance)
+           {
+                   distance = newDistance;
+                   c = i;
+
+           }
+       }
+   }   
+   if(c >=0 )
+   {
+       hitColliders[c].GetComponent<SwitchingLevels>().CombineChess(_selected);
+   }
+
+}
+private void OnLeftClick(InputAction.CallbackContext context)
+{
+   Vector2 pos = grapInput.Mouse.Position.ReadValue<Vector2>();
+   ray = camera.ScreenPointToRay(pos);
+   RaycastHit hit;
+   if( Physics.Raycast(ray, out hit))
+   {
+       Transform selection = hit.transform;
+       if (selection.tag == "Chess")
+       {
+           _selecting = true;
+           _selected = hit.transform;
+       }
+   }
+}
+private void MovingSelected()
+{
+   if (_selecting == true)
+   {
+       mZCoord =camera.WorldToScreenPoint(_selected.transform.position).z;
+       Vector2 pos = grapInput.Mouse.Position.ReadValue<Vector2>();
+       Vector3 mousePosition = new Vector3(pos.x, pos.y, mZCoord);
+       _mOffSet = camera.ScreenToWorldPoint(mousePosition);
+       _selected.transform.position =  _mOffSet;
+   }    
+}
+*/
     // Update is called once per frame
     void Update()
     {
