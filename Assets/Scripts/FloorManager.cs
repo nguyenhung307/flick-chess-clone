03cecb2ee6022768a355317584c2a5de47d2529d
Cong <lahuycong2000@gmail.com>
2022-05-09 11:20:17 +0700

    _push_update

--- Assets/Scripts/FloorManager.cs
+++ Assets/Scripts/FloorManager.cs
@@ -7,6 +7,8 @@ public class FloorManager : MonoBehaviour
     public static FloorManager Instance;
     [SerializeField] private SpawnFloor _spawnFloor;
     public SpawnFloor SpawnFloor => _spawnFloor;
+    [SerializeField] private SpawnFloor _spawnFloor2;
+    public SpawnFloor SpawnFloor2 => _spawnFloor2;
     [SerializeField] private List<Floor> _listFloor = new List<Floor>();
     public List<Floor> ListFloor => _listFloor;
     
