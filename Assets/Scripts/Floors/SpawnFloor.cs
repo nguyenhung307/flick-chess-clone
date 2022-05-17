6509e5fa5787ad25da6ffea1794872d7fedddf31
Cong <lahuycong2000@gmail.com>
2022-05-17 15:20:05 +0700

    _push

--- Assets/Scripts/Floors/SpawnFloor.cs
+++ Assets/Scripts/Floors/SpawnFloor.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class SpawnFloor : MonoBehaviour
 {
+    public int LevelGame;
+    private int _numberObject;
     [SerializeField] private Transform _spawnPos;
     private void Start()
     {
-        // StartCoroutine(IeSpawn());
-        Spawn();
+        _numberObject = LevelGame - 1;
+        if (_numberObject > FloorManager.Instance.ListFloor.Count)
+        {
+            SpawnFloorRandom();
+        }
+        else
+        {
+            SpawnFloorLv();
+        }
     }
-    public void Spawn()
+    public void SpawnFloorLv()
+    {
+        Vector3 pos = _spawnPos.transform.position;
+
+        GameObject go = Instantiate(FloorManager.Instance.ListFloor[_numberObject].gameObject);
+        go.transform.position = pos;
+    }
+    public void SpawnFloorRandom()
     {
         int floor = Random.Range(0, FloorManager.Instance.ListFloor.Count);
         Vector3 pos = _spawnPos.transform.position;
 
         GameObject go = Instantiate(FloorManager.Instance.ListFloor[floor].gameObject);
         go.transform.position = pos;
-
-
     }
 
 }
